b9cf5e7f49875ec2b1c66df758b2bbb2b85fa2ea
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-12-07 16:00:56 -0500

    perf: :zap: Supervised consumer and producer stages

--- lib/fake_scrapper/application.ex
+++ lib/fake_scrapper/application.ex
@@ -8,8 +8,8 @@ defmodule FakeScrapper.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: FakeScrapper.Worker.start_link(arg)
-      # {FakeScrapper.Worker, arg}
+      FakeScrapper.PageProducer,
+      FakeScrapper.PageConsumer
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
