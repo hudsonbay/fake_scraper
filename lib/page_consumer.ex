39ef265c3e454d77e5ed2acf59caea3f69ed47b6
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-12-07 15:57:02 -0500

    feat: :sparkles: Created a producer

---
+++ lib/page_consumer.ex
@@ -0,0 +1,23 @@
+defmodule FakeScrapper.PageProducer do
+  use GenStage
+  require Logger
+
+  def start_link(_args) do
+    initial_state = []
+    GenStage.start_link(__MODULE__, initial_state)
+  end
+
+  def init(intital_state) do
+    Logger.info("PageConsumer init")
+    {:consumer, intital_state, subscribe_to: [PageProducer]}
+  end
+
+  def handle_events(events, _from, state) do
+    Logger.info("PageConsumer received #{inspect(events)}")
+
+    # Pretending we're scraping webpages
+    Enum.each(events, fn _page -> FakeScrapper.work() end)
+
+    {:noreply, [], state}
+  end
+end
