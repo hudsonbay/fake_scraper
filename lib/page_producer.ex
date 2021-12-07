d67df2a4af8744e9fa2230aa7983a3f43cfc0a67
hudsonbay <manuelmenendezalfonso@gmail.com>
2021-12-06 23:05:26 -0500

    feat: :sparkles: Created a producer

---
+++ lib/page_producer.ex
@@ -0,0 +1,20 @@
+defmodule FakeScrapper.PageProducer do
+  use GenStage
+  require Logger
+
+  def start_link(_args) do
+    initial_state = []
+    GenStage.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
+  def init(initial_state) do
+    Logger.info("PagerProducer init")
+    {:producer, initial_state}
+  end
+
+  def handle_demand(demand, state) do
+    Logger.info("PageProducer received demand for #{demand} pages")
+    events = []
+    {:noreply, events, state}
+  end
+end
