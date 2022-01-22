e9887bfc7d4a4321bba3195c14a37d941931b1f8
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-01-22 00:30:12 -0500

    refactor: :fire: Simplified consumer

--- lib/page_consumer.ex
+++ lib/page_consumer.ex
@@ -1,25 +1,9 @@
 defmodule PageConsumer do
-  use GenStage
   require Logger
 
-  def start_link(_args) do
-    initial_state = []
-    GenStage.start_link(__MODULE__, initial_state)
-  end
+  def start_link(event) do
+    Logger.info("PageConsumer received #{event}")
 
-  def init(intital_state) do
-    Logger.info("PageConsumer init")
-
-    sub_opts = [{PageProducer, min_demand: 0, max_demand: 1}]
-    {:consumer, intital_state, subscribe_to: sub_opts}
-  end
-
-  def handle_events(events, _from, state) do
-    Logger.info("PageConsumer received #{inspect(events)}")
-
-    # Pretending we're scraping webpages
-    Enum.each(events, &(FakeScrapper.work(&1)))
-
-    {:noreply, [], state}
+    Task.start_link(fn -> FakeScrapper.work(event) end)
   end
 end
